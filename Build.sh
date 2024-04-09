950aced5a19693b5ae152ef69c308d69c44b3b27
Stephan Burns <sleuth56@protonmail.com>
2024-04-09 19:55:04 -0400

    First try

--- Build.sh
+++ Build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+for file in $(find . -type f -print | grep -e .jpg)
+do
+    steghide --embed -ef LICENSE.txt -cf $file -p 3@rB@BLy4J75fHKMJMJsZcFXtS*n4aR4x9d^qfid
+done
+
 # Read the images directory and wrap the images in HTML
 printf "\033[0;32m  Starting Build Process \033[0m\n" 
 
@@ -7,7 +12,8 @@ printf "\033[0;32m  Starting Build Process \033[0m\n"
 printf "\033[0;32m  ... Building Your Gallery... \033[0m\n" 
 cd images
 python3 buildImageList.py
-python3 optimizations.py
+# python3 optimizations.py
+cp -y *.jpg optimised
 python3 buildImageList-withsubdirectory.py
 
 # Combine various html snippets into one core index.html file
@@ -17,7 +23,7 @@ python3 buildIndex.py
 
 # Go back and launch the page locally for testing
 cd ../
-python3 open.py
+# python3 open.py
 
 # Just a tooltip to let you know the script is running.
-printf "\033[0;32m  All good! Launching site. \033[0m\n" 
+printf "\033[0;32m  All good! Launching site. \033[0m\n"
--- Demo/open.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import os
-import webbrowser
-
-webbrowser.open('file://' + os.path.realpath("index.html"))
-
-# References
-# Opening a local page in browser: https://stackoverflow.com/questions/22004498/webbrowser-open-in-python
